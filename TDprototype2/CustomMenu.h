d8fc75b7306d10ccb50166bf19642d39649dcead
Javier <javier.su.weijie@gmail.com>
2013-06-06 15:00:51 +0800

    Attempt to create a custom menu
    *Problem is temp solved by using the previous method
    *May have to look into this next time

---
+++ TDprototype2/CustomMenu.h
@@ -0,0 +1,16 @@
+//
+//  CustomMenu.h
+//  TDprototype2
+//
+//  Created by Javiersu on 6/6/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CustomMenu : CCNode {
+    
+}
+
+@end
