a339c018618a97d65b4a125a74803e9af437f19c
Javier <javier.su.weijie@gmail.com>
2013-06-12 23:01:49 +0800

    Added IceBeamTower

---
+++ TDprototype2/Structure/IceBeamTower.h
@@ -0,0 +1,17 @@
+//
+//  IceBeamTower.h
+//  TDprototype2
+//
+//  Created by Javiersu on 11/6/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "Structure.h"
+
+@interface IceBeamTower : Structure {
+    
+}
+
+@end
---
+++ TDprototype2/Structure/IceBeamTower.m
@@ -0,0 +1,14 @@
+//
+//  IceBeamTower.m
+//  TDprototype2
+//
+//  Created by Javiersu on 11/6/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "IceBeamTower.h"
+
+
+@implementation IceBeamTower
+
+@end
