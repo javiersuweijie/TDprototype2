3382cd0778d8e53a9d7b582401c3946270bfd73b
Javier <javier.su.weijie@gmail.com>
2013-07-21 23:31:44 +0800

    init commit for upgrademenu

---
+++ TDprototype2/UpgradeMenu.h
@@ -0,0 +1,16 @@
+//
+//  UpgradeMenu.h
+//  TDprototype2
+//
+//  Created by Javiersu on 21/7/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface UpgradeMenu : CustomMenu {
+    
+}
+
+@end
---
+++ TDprototype2/UpgradeMenu.m
@@ -0,0 +1,14 @@
+//
+//  UpgradeMenu.m
+//  TDprototype2
+//
+//  Created by Javiersu on 21/7/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "UpgradeMenu.h"
+#import "CustomMenu.h"
+
+@implementation UpgradeMenu
+
+@end
