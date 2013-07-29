4c5587296446a00bb360af89d96c634a35fb11d8
Javier <javier.su.weijie@gmail.com>
2013-07-29 09:22:28 +0800

    Smart walls added

--- TDprototype2/Wall.m
+++ TDprototype2/Wall.m
@@ -13,12 +13,16 @@
 @implementation Wall
 static int cost = 0;
 static NSMutableArray* wallArray;
+static NSMutableDictionary* midWallDict;
 id filledNode;
 
 -(id)initWithPosition:(CGPoint)point
 {
     if (!wallArray) {
         wallArray = [[NSMutableArray alloc]init];
+    }
+    if (!midWallDict) {
+        midWallDict = [[NSMutableDictionary alloc]init];
     }
     if ([super initWithFile:[NSString stringWithFormat:@"Wall.png"]]) {
 //        [self setColor:ccc3(189, 195, 199)];
@@ -40,6 +44,7 @@ id filledNode;
 
 -(void)setPosition:(CGPoint)position
 {
+    [self removeMidWall];
     [super setPosition:position];
     [self checkAdj];
 }
@@ -52,28 +57,31 @@ id filledNode;
             continue;
         }
         if (CGPointEqualToPoint([wall getGrid],ccpAdd(selfGrid, ccp(0,1)))) {
-            NSLog(@"found adj wall1");
             CCSprite* midWall = [CCSprite spriteWithFile:@"WallCRight.png"];
             [midWall setPosition:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-1,2))])];
             [filledNode addChild:midWall z:-self.position.y-self.contentSize.height/4];
+            [midWallDict setObject:midWall forKey:[NSValue valueWithCGPoint:midWall.position]];
         }
         if (CGPointEqualToPoint([wall getGrid],ccpAdd(selfGrid, ccp(1,0)))) {
             NSLog(@"found adj wall1");
             CCSprite* midWall = [CCSprite spriteWithFile:@"WallCLeft.png"];
             [midWall setPosition:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(0,1))])];
             [filledNode addChild:midWall z:-self.position.y+self.contentSize.height/4];
+            [midWallDict setObject:midWall forKey:[NSValue valueWithCGPoint:midWall.position]];
         }
         if (CGPointEqualToPoint([wall getGrid],ccpAdd(selfGrid, ccp(-1,0)))) {
             NSLog(@"found adj wall1");
             CCSprite* midWall = [CCSprite spriteWithFile:@"WallCLeft.png"];
             [midWall setPosition:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-2,1))])];
             [filledNode addChild:midWall z:-self.position.y-self.contentSize.height/4];
+            [midWallDict setObject:midWall forKey:[NSValue valueWithCGPoint:midWall.position]];
         }
         if (CGPointEqualToPoint([wall getGrid],ccpAdd(selfGrid, ccp(0,-1)))) {
             NSLog(@"found adj wall1");
             CCSprite* midWall = [CCSprite spriteWithFile:@"WallCRight.png"];
             [midWall setPosition:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-1,0))])];
             [filledNode addChild:midWall z:-self.position.y+self.contentSize.height/4];
+            [midWallDict setObject:midWall forKey:[NSValue valueWithCGPoint:midWall.position]];
         }
     }
 }
@@ -81,5 +89,24 @@ id filledNode;
 -(CGPoint)getGrid
 {
     return [IsometricOperator gridNumber:self.position];
+}
+
+-(void)removeMidWall
+{
+    CGPoint selfGrid = [self getGrid];
+    NSArray* array = [NSArray arrayWithObjects:
+                      [NSValue valueWithCGPoint:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-1,0))])],
+                      [NSValue valueWithCGPoint:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-2,1))])],
+                      [NSValue valueWithCGPoint:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(0,1))])],
+                      [NSValue valueWithCGPoint:ccpMidpoint(self.position, [IsometricOperator gridToCoord:ccpAdd(selfGrid, ccp(-1,2))])],
+                      nil];
+    
+    for (id value in array) {
+        id midWall = [midWallDict objectForKey:value];
+        if (midWall) {
+//            [midWallDict removeObjectForKey:value];
+            [midWall removeFromParentAndCleanup:YES];
+        }
+    }
 }
 @end
