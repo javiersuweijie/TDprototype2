b61d44bf32fc1ddf6865fda652dcb491c3576c7f
Javier <javier.su.weijie@gmail.com>
2013-07-30 17:19:45 +0800

    Added a menu bar to spawn units

---
+++ TDprototype2/UI/UnitMenu.h
@@ -0,0 +1,16 @@
+//
+//  UnitMenu.h
+//  TDprototype2
+//
+//  Created by Javiersu on 30/7/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface UnitMenu : CustomMenu {
+    
+}
+
+@end
---
+++ TDprototype2/UI/UnitMenu.m
@@ -0,0 +1,14 @@
+//
+//  UnitMenu.m
+//  TDprototype2
+//
+//  Created by Javiersu on 30/7/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "UnitMenu.h"
+
+
+@implementation UnitMenu
+
+@end
